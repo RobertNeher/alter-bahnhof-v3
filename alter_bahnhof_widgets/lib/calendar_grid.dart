4235727fdc3fc09dc304433df43d9b08dae465ef
rneher <robert.neher@bartscher-hasenaecker.de>
2024-02-21 09:01:38 +0100

    new widget Calendar Grid basing upon MonthCalendar

---
+++ alter_bahnhof_widgets/lib/calendar_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:alter_bahnhof_widgets/month_calendar.dart';
+
+class CalendarGrid extends StatelessWidget {
+  int columns = 2;
+  String startMonth = '';
+  int numberOfMonths = 6;
+  VoidCallback Function() callback;
+  CalendarGrid(
+      {super.key,
+      this.columns = 2,
+      this.startMonth = '',
+      this.numberOfMonths = 12,
+      required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    int year = int.parse(startMonth.substring(0, 4));
+    int month = int.parse(startMonth.substring(5, 7));
+    return GridView.count(
+      childAspectRatio: 1.8,
+      crossAxisCount: columns,
+      shrinkWrap: true,
+      children: List.generate(
+          numberOfMonths,
+          (index) => MonthCalendar(
+                month: DateTime(year, month + index, 1),
+                managementView: true,
+                callback: callback,
+              )),
+    );
+  }
+}
